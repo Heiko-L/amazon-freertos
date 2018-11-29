f77348c608fa0e12c87503c589ceda5f8f5eacbb
Heiko-L <no.spam.to.me@ish.de>
2018-11-29 15:32:42 +0100

    Added atmega88 to CMake/MicroprocessorSpecific.cmake

--- CMake/MicroprocessorSpecific.cmake
+++ CMake/MicroprocessorSpecific.cmake
@@ -1,6 +1,12 @@
 # set uc specific sources and includes
 if(CMAKE_SYSTEM_PROCESSOR STREQUAL "ATmega323")
-	include_directories(source/port/GCC/ATMega323)
-	set(INSTALL_HEADERS ${INSTALL_HEADERS} source/port/GCC/ATMega323/portmacro.h)
-	set(OPTIONAL_SOURCE_FILES ${OPTIONAL_SOURCE_FILES} source/port/GCC/ATMega323/port.c)
+	include_directories(port/ATMega323/include)
+	set(INSTALL_HEADERS ${INSTALL_HEADERS} port/ATMega323/include/FreeRTOS/portmacro.h)
+	set(OPTIONAL_SOURCE_FILES ${OPTIONAL_SOURCE_FILES} port/ATMega323/source/port.c)
 endif(CMAKE_SYSTEM_PROCESSOR STREQUAL "ATmega323")
+
+if(CMAKE_SYSTEM_PROCESSOR STREQUAL "atmega88")
+	include_directories(port/atmega88/include)
+	set(INSTALL_HEADERS ${INSTALL_HEADERS} port/atmega88/include/FreeRTOS/portmacro.h)
+	set(OPTIONAL_SOURCE_FILES ${OPTIONAL_SOURCE_FILES} port/atmega88/source/port.c)
+endif(CMAKE_SYSTEM_PROCESSOR STREQUAL "atmega88")
