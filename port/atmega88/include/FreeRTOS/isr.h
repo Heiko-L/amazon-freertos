a43a2ac4b9bb16a42234ae984d5c750c8605a780
Heiko-L <no.spam.to.me@ish.de>
2018-11-29 15:27:05 +0100

    Added atmega88 port

---
+++ port/atmega88/include/FreeRTOS/isr.h
@@ -0,0 +1,35 @@
+#include <avr/interrupt.h>
+#include <FreeRTOS/RTOSConfig.h>
+#include <FreeRTOS/context_macros.h>
+#include <FreeRTOS/common.h>
+#include <FreeRTOS/task.h>
+
+#if configUSE_PREEMPTION == 1
+
+	/*
+	 * Tick ISR for preemptive scheduler.  We can use a naked attribute as
+	 * the context is saved at the start of portSAVE_CONTEXT().  The tick
+	 * count is incremented after the context is saved.
+	 */
+	ISR(PORT_TIMER_INTERRUPT_VECTOR, ISR_NAKED)
+	{
+	    portSAVE_CONTEXT();
+	    if( xTaskIncrementTick() != pdFALSE )
+	    {
+		vTaskSwitchContext();
+	    }
+	    portRESTORE_CONTEXT();
+	    asm volatile ( "reti" );
+	}
+#else
+
+	/*
+	 * Tick ISR for the cooperative scheduler.  All this does is increment the
+	 * tick count.  We don't need to switch context, this can only be done by
+	 * manual calls to taskYIELD();
+	 */
+	ISR(PORT_TIMER_INTERRUPT_VECTOR)
+	{
+		xTaskIncrementTick();
+	}
+#endif
