4c23b15622d1c7017d39fd63088d1315f4da155d
Heiko-L <no.spam.to.me@ish.de>
2018-12-18 14:59:01 +0100

    Added ATMega168

--- CMake/MicroprocessorSpecific.cmake
+++ CMake/MicroprocessorSpecific.cmake
@@ -8,3 +8,7 @@ endif(CMAKE_SYSTEM_PROCESSOR STREQUAL "ATmega323")
 if(CMAKE_SYSTEM_PROCESSOR STREQUAL "atmega88")
 	add_subdirectory (port/atmega88)
 endif(CMAKE_SYSTEM_PROCESSOR STREQUAL "atmega88")
+
+if(CMAKE_SYSTEM_PROCESSOR STREQUAL "atmega168")
+	add_subdirectory (port/atmega88)
+endif(CMAKE_SYSTEM_PROCESSOR STREQUAL "atmega168")
