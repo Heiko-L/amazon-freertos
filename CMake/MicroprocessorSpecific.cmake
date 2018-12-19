bd856a94e576a682492d3c798e219e6c27331811
Heiko-L <no.spam.to.me@ish.de>
2018-12-19 10:09:01 +0100

    Removed ATMega323 target

--- CMake/MicroprocessorSpecific.cmake
+++ CMake/MicroprocessorSpecific.cmake
@@ -1,10 +1,4 @@
 # set uc specific sources and includes
-if(CMAKE_SYSTEM_PROCESSOR STREQUAL "ATmega323")
-	include_directories(port/ATMega323/include)
-	set(INSTALL_HEADERS ${INSTALL_HEADERS} port/ATMega323/include/FreeRTOS/portmacro.h)
-	set(OPTIONAL_SOURCE_FILES ${OPTIONAL_SOURCE_FILES} port/ATMega323/source/port.c)
-endif(CMAKE_SYSTEM_PROCESSOR STREQUAL "ATmega323")
-
 if(CMAKE_SYSTEM_PROCESSOR STREQUAL "atmega88")
 	add_subdirectory (port/atmega88)
 endif(CMAKE_SYSTEM_PROCESSOR STREQUAL "atmega88")
